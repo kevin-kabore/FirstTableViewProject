b7190a34c31348b697bad2473f3b03f3ce8f9087
Kevin Kabore <kevinkabore@kevins-macbook-pro.local>
2016-03-22 22:19:23 -0400

    Initial Commit

---
+++ FirstTableViewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstTableViewProject
+//
+//  Created by Kevin Kabore on 3/22/16.
+//  Copyright © 2016 Kevin Kabore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
